5183e9a6e1011122567b5efe331d38f07c287bad
Mikhail-Mart <76031384+Mikhail-Mart@users.noreply.github.com>
2021-06-29 19:36:58 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -5,3 +5,15 @@ text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
 # TODO: your code here
+x = 0
+sum = 0
+lengh_text = len(text)
+while x <= lengh_text:
+    y = text.find(" ", x)
+    print("len x", x, y)
+    if y == -1:
+        y = lengh_text
+    elif y-x>5:
+        sum += 1
+    x = y+1
+print("количество слов", sum)
